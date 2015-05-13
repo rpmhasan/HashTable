76d87353ba68378bf8016fb6f5a708913a1779d8
rpmhasan <rpm.hasan@gmail.com>
2015-05-13 16:43:51 +0600

    Full

---
+++ HashTableApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable htable = new Hashtable();
+            htable.Add(1, 10);
+            htable[2] = 11;
+            htable[3] = 12;
+            htable[4] = "Rpm";
+
+            ICollection l = htable.Keys;
+            foreach (var i in l)
+            {
+                Console.WriteLine(htable[i]);
+            }
+            Console.Read();
+
+
+            //Console.WriteLine(htable.Contains(3));
+            //Console.Read();
+
+
+            //Console.WriteLine(htable.ContainsValue("Rpm"));
+            //Console.Read();
+
+
+            //Console.WriteLine(htable.Count);
+            //Console.Read();
+        }
+    }
+}
